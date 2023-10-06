bb8f4da55a41e22635f4e0b253fbdc990e1c5a7b
reg-edit <99992948+reg-edit@users.noreply.github.com>
2023-10-06 12:03:24 +0100

    Place some temporary belts and a background

--- conveyorGame/js/sceneMain.js
+++ conveyorGame/js/sceneMain.js
@@ -6,14 +6,18 @@ class SceneMain extends Phaser.Scene {
     {
         //load images or sounds
         this.load.image("box", "images/box.png")
+        this.load.image("background", "images/background.png")
+        this.load.image("belt1", "images/l2r1.png")
 
        
     }
     create()
     {
         //define objects
-        //this.box=this.height.image(10)
-        this.box=this.add.image(0,game.config.height/2, "box")
+        this.background = this.add.image(550,20, "background").setScale(.4)
+        this.belt = this.add.image(game.config.width/4,(game.config.height/2)+20, "belt1").setScale(.35)
+        this.belt = this.add.image((game.config.width/4)+80,(game.config.height/2)+20, "belt1").setScale(.35)
+        this.box=this.add.image(game.config.width/4,game.config.height/2, "box").setScale(.2)
 
     }
     update(){
