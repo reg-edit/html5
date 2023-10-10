381e49ec5c6ee564a5f2f165c72f67ab5af46402
reg-edit <99992948+reg-edit@users.noreply.github.com>
2023-10-10 16:35:35 +0100

    Change game title and add a function to make lengths of belt.

--- conveyorGame/js/sceneMain.js
+++ conveyorGame/js/sceneMain.js
@@ -4,7 +4,6 @@ class SceneMain extends Phaser.Scene {
     }
     preload() //load images or sounds
     {
-        
         this.load.image("box", "images/box.png")
         this.load.image("background", "images/background.png")
         this.load.image("machine", "images/machine.png")
@@ -18,21 +17,30 @@ class SceneMain extends Phaser.Scene {
         this.load.image("tBelt2", "images/b2r2.png")
         this.load.image("tBelt3", "images/b2r3.png")
         this.load.image("tBelt4", "images/b2r4.png")
-
-       
     }
     create() //define objects
     {
 
-        //Scale
+        /* Functions */
+        function createBelts(x,y,quantity,scene) {
+            let firstBeltX = x
+            let currentBeltX = firstBeltX+(256*beltScale)
+    
+            scene.add.sprite(firstBeltX,y,"belt1").play("l2rAnim").setScale(beltScale)
+            for(let i = 0; i < quantity-1; i++){
+                scene.add.sprite(currentBeltX,y, "belt1").play("l2rAnim").setScale(beltScale)
+                currentBeltX = currentBeltX+(256*beltScale)
+            }
+        }
+
+        /* Variables */
         let beltScale = .4
         let machScale = .25
         let boxScale = .2
         let beltSpeed = 12
-        let beltAmount = 6
 
 
-        //Belt Animations
+        /* Animations */
         this.anims.create({  //standard belt
             key:"l2rAnim",
             frames: [
@@ -57,68 +65,43 @@ class SceneMain extends Phaser.Scene {
             repeat: -1
         })
 
+
+        /* Asset placement */
         //Background
         this.background = this.add.image(0,-440, "background").setScale(.6,.55).setOrigin(0,0)
 
 
         //Belt Placement
-        let firstBeltx = 0
-        let currentBeltx = firstBeltx+(256*beltScale)
-
-
-        this.add.sprite(firstBeltx,150, "belt1").play("l2rAnim").setScale(beltScale)
-
-        for(let i = 0; i < beltAmount; i++){
-            this.add.sprite(currentBeltx,150, "belt1").play("l2rAnim").setScale(beltScale)
-            currentBeltx = currentBeltx+(256*beltScale)
-        }
-        
-
-        //Machine Placement
-        const machine = this.add.image(currentBeltx,150, "machine").setScale(machScale)
-
-
-   
-
-
-        //this.add.sprite(1100,150, "belt1").play("l2rAnim").setScale(beltScale)
-
-        //this.add.sprite(1015,40, "tBelt1").play("b2rAnim").setScale(beltScale)
-
+        createBelts(0, 150,6,this)
+        //this.add.sprite(1015,40, "tBelt1").play("b2rAnim").setScale(beltScale) /*corner belt*/
 
         //Boxes
+        this.box = this.add.image(game.config.width/4,game.config.height/2, "box").setScale(boxScale)
+    
 
-        let box = this.add.image(game.config.width/4,game.config.height/2, "box").setScale(boxScale)
-
-        
-
-        // Graphics
-
-
-
+        //Machine Placement
+        this.machine = this.add.image(600,150, "machine").setScale(machScale)
 
         //Controllers
+        // this.input.keyboard.on('keydown-D', event =>
+        // {
+        //      box.x += 4;
+        // });
 
-        this.input.keyboard.on('keydown-D', event =>
-        {
-             box.x += 4;
-        });
-
-        this.input.keyboard.on('keydown-W', event =>
-        {
-             beltSpeed += 4;
-             console.log(beltSpeed)
-        });
+        // this.input.keyboard.on('keydown-W', event =>
+        // {
+        //      beltSpeed += 4;
+        //      console.log(beltSpeed)
+        // });
 
 
         //Text
         this.textTag = this.add.text(10,10, "Automate the SHIP out of it!",{fontFamily:'Sora',fontSize:40,fontStyle:800}).setStroke("black",3)
         
-        
+
     }
     update() //constant running loop
     {
-        
         
         
     }
